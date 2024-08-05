1d314815c67f8b81a515227c48f0b872faa4d438
dkdkdsa <bos94330@gmail.com>
2024-08-05 14:04:35 +0900

    PrefabData

--- Assets/02_Script/Core/Network/Data/PrefabData.cs
+++ Assets/02_Script/Core/Network/Data/PrefabData.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections;
 using Unity.Netcode;
-using UnityEngine;
 
 public struct PrefabData : INetworkSerializable
 {
