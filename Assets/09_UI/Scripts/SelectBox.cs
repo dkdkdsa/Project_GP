0fca8712dc4b2ce148c643122eaa457ef21f5718
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-08-13 00:57:30 +0900

    UI

---
+++ Assets/09_UI/Scripts/SelectBox.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectBox : UIComponent, IMouseEventHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("오오오오옹ㄴ");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("오오오오옹ㄴ");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("오오오오옹ㄴ");
+    }
+}
