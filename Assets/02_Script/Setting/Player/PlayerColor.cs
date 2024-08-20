ff0379bb02def21cbdb9676d83aab8d9cea9dace
rlatjdgh1122 <98933564+rlatjdgh1122@users.noreply.github.com>
2024-08-20 14:55:18 +0900

    색깔놀이

---
+++ Assets/02_Script/Setting/Player/PlayerColor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerColor
+{
+    public static Color GetColor(PlayerColorType type)
+    {
+        return PlayerColor.GetColor(type);
+    }
+}
---
+++ Assets/02_Script/Setting/Player/PlayerColorType.cs
@@ -0,0 +1,11 @@
+public enum PlayerColorType
+{
+    None = 0,
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    //...
+    End,
+
+}
